9b1b7c2e78cfa5397e4f5d693e98a08b69844035
adamhuan <dkseven_eli@163.com>
2019-04-01 20:19:30 +0800

    增加了：自动爬取图片的功能
    知乎的文章的很多配图的质量都很高，我也想要。

--- zhihu.py
+++ zhihu.py
@@ -3,9 +3,6 @@
 
 # 系统相关
 import os
-import sys
-import json
-import time
 
 # JSON
 import json
@@ -66,13 +63,63 @@ if __name__ == "__main__":
 
     # 要抓取的网页
     target_url = "https://www.zhihu.com/question/306126586/answer/607889154"
+    target_url = "https://zhuanlan.zhihu.com/p/61062475"
+    target_url = "https://www.zhihu.com/question/25528331/answer/76404492"
+    target_url = "https://zhuanlan.zhihu.com/p/61015286"
+    target_url = "https://www.zhihu.com/question/314984303/answer/637564381"
 
     # do
     wd = web_discovery(target_url)
 
-    # variable - level 2
-    listVideo = wd.ObjBeautifulSoup.findAll('a',attrs={"class" : "video-box"})
+    # variable - level 1 - 问题属性
+    #QuestionHeader_title = wd.ObjBeautifulSoup.findAll('h1',attrs={"class" : "QuestionHeader-title"})
+    #QuestionHeader_title = wd.ObjBeautifulSoup.find('h1', class_="QuestionHeader-title").get_text()
+    QuestionHeader_title = wd.ObjBeautifulSoup.find('title').get_text()
 
+    # variable - level 2 - 资源列表
+    # 所有视频
+    listVideo = wd.ObjBeautifulSoup.findAll('a',attrs={"class" : "video-box"})
+    # 所有图片
+    listPic = wd.ObjBeautifulSoup.findAll('img',attrs={"class" : "origin_image zh-lightbox-thumb"})
+
+    # display
+    print("##########################################")
+    print("标题：【" + QuestionHeader_title + "】")
+
+    # 如果目录不存在，就创建目录
+    if not os.path.exists(download_directory + QuestionHeader_title + "/"):
+        os.mkdir(download_directory + QuestionHeader_title + "/")
+
+    # 下载图片
+    for pic in listPic:
+
+        # variable
+        pic_url = pic.get('data-original')
+        pic_name = str(pic_url[::-1]).split('/')[0][::-1]
+
+        # 执行文件下载阶段
+        download_path = download_directory + QuestionHeader_title + "/图片/" + pic_name
+
+        # 如果目录不存在，就创建目录
+        if not os.path.exists(download_directory + QuestionHeader_title + "/图片/"):
+            os.mkdir(download_directory + QuestionHeader_title + "/图片/")
+
+        # display
+        print("==============================================")
+        print("图片地址：【" + pic_url + "】")
+        print("图片名称：【" + pic_name + "】")
+        print("保存地址: 【" + download_path + "】")
+        print("------------")
+        print("Execute：【Download】= Begin")
+
+        # do
+        wd.ObjDownload.urlretrieve(pic_url, download_path)
+
+        # display
+        print("Execute：【Download】= Done")
+        print("")
+
+    # 下载视频
     for video in listVideo:
 
         # variable
@@ -98,7 +145,11 @@ if __name__ == "__main__":
         download_url = download_data_json['playlist']['LD']['play_url']
 
         # 执行文件下载阶段
-        download_path = download_directory + purpose_url_id + ".mp4"
+        download_path = download_directory + QuestionHeader_title + "/视频/" + purpose_url_id + ".mp4"
+
+        # 如果目录不存在，就创建目录
+        if not os.path.exists(download_directory + QuestionHeader_title + "/视频/"):
+            os.mkdir(download_directory + QuestionHeader_title + "/视频/")
 
         # display
         print("==============================================")
